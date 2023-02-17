0dd9d74c3b6b25459497f9b8a715f0f2a9cfefd8
Irene06he <irene.hehe34@myhunter.cuny.edu>
2023-02-17 05:52:17 -0600

    modified task a

--- main.cpp
+++ main.cpp
@@ -11,8 +11,8 @@ int main()
 {
 
 //Test for task A 
-  	std::cout<<"\n\n"<<"Date: 03/16/2018"<<" "<<"East basin storage:"<<get_east_storage("03/16/2018")<<"\n\n";
-  	std::cout<<"Date: 05/20/2018"<<" "<<"East basin storage:"<<get_east_storage("05/20/2018")<<"\n\n";
+  	std::cout<<"\n\n"<<"Date: 03/16/2018"<<" "<<"East basin storage:"<<get_east_storage("03/16/2018")<<"  billion gallons"<<"\n\n";
+  	std::cout<<"Date: 05/20/2018"<<" "<<"East basin storage:"<<get_east_storage("05/20/2018")<<"  billion gallons"<<"\n\n";
   	
  //Test for task B
  	std::cout<<"Minimum storage in East basin: "<<" " <<get_min_east( )<<" "<<"billion gallons"<<"\n\n";
